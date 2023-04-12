a92e0f39929c0b63db99639ed9822b126a078271
Luca Corvino <l.corvino@live.it>
2023-04-12 18:49:25 +0200

    deploy

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,7 +6,7 @@ import { routes } from './routes';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes,{ useHash: true })
   ],
   declarations: [],
   exports:[
