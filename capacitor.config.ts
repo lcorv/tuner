b1a0983ded2ab7e114748f6c29e46040d7c0f8ef
Luca Corvino <l.corvino@live.it>
2023-04-13 00:29:21 +0200

    deploy 4

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.android.tuner',
+  appName: 'tuner',
+  webDir: 'dist/tuner',
+  bundledWebRuntime: false
+};
+
+export default config;
