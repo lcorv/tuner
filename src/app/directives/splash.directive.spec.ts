2e968eeb4691b5aa3fa7120e0293a958cfd243c6
lcorv <108190944+lcorv@users.noreply.github.com>
2023-04-12 17:51:29 +0200

    Delete src directory

--- src/app/directives/splash.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SplashDirective } from './splash.directive';
-
-describe('SplashDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SplashDirective();
-    expect(directive).toBeTruthy();
-  });
-});
